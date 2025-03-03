ddce8ac23a8c40b6ee205663bfc50a3c01f1b4c1
OzelHD <davide0929carocci@gmail.com>
2025-03-03 13:07:35 +0100

    Good commit

--- Day6.py
+++ Day6.py
@@ -4,3 +4,19 @@
 # 3️⃣ Creates a Student class (inherits from Person) and adds an extra attribute for grade.
 # 4️⃣ Overrides the method to also display the student's grade.
 
+class person:
+    name = str(input("name"))
+    age = int(input("Age"))
+
+print(person.name)
+
+# def fkt(x, y):
+#     x = x+2
+#     y= y+1
+#     z = x+y
+#     a = z + y + x
+#     return x, y, z, a
+
+# x,y =int(input("x ")), int(input("y "))
+# X, Y, Z, A = fkt(x,y)
+# print(str(X), str(Y), str(Z), str(A))
